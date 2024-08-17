7515c0df4dab54e0bb8255d35a1d0123eba2f5bb
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-08-17 19:13:16 +0100

    ere

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -11,7 +11,7 @@ export default function ContactPage({ contact }) {
   return (
     <>
       <Headers />
-      {contact && contact._id ? (
+      {contact ? (
         <ContactContainer data={contact} />
       ) : (
         <HomeSkeleton />
