42ecde944e75c5c92ca7c1d3d64235634009079b
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-08-29 08:53:14 +0100

    update

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,8 @@ import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title: "Champion Logistics",
-  description: "",
+  description:
+    "Champion Logistics provides comprehensive logistics solutions including land freight, sea freight, air freight, warehousing, and courier services. With a global network and a commitment to timely, secure, and cost-effective deliveries, we streamline your supply chain for both domestic and international shipments. Whether you're moving goods across borders or need secure storage, Champion Logistics ensures your cargo reaches its destination with efficiency and care.",
 };
 
 export default function RootLayout({ children }) {
